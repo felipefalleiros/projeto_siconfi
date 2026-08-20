8a7b555ceea9b1b0957d3ebd17926aa4f56993c0
felipefalleiros <mendesf84@gmail.com>
2026-08-20 13:48:08 -0300

    feat: exibir análise de verificações

--- app/pages/analise_das_verificacoes.py
+++ app/pages/analise_das_verificacoes.py
@@ -13,6 +13,31 @@ def carregar_dados():
 df = carregar_dados()
 
 
+def calcular_media_estadual(df, exercicio, estado):
+    df_filtrado = df[
+        (df['exercicio'] == exercicio) &
+        (df['estado'] == estado) &
+        (df['aplicavel'] == True)
+    ]
+
+    return (
+        df_filtrado
+        .groupby('verificacao', as_index=False)['nota']
+        .mean()
+        .rename(columns={'nota': 'media_estadual'})
+    )
+    
+def destacar_nota(row):
+    cor = (
+        "green"
+        if row["nota"] >= row["media_estadual"]
+        else "red"
+    )
+
+    return pd.Series({
+        "nota": f"color: {cor}"
+    })
+
 st.title("Análise das Verificações")
 
 # -------------------------
@@ -63,10 +88,71 @@ municipio_selecionado = st.selectbox(
 )
 
 # -------------------------
+# Filtro de dimensão
+# -------------------------
+
+dimensoes = sorted(
+    df["dimensao"].unique()
+)
+
+dimensao_selecionada = st.selectbox(
+    "Dimensão",
+    dimensoes
+)
+
+# -------------------------
 # Dados do município
 # -------------------------
 
 df_municipio = df_estado[
     (df_estado["municipio"] == municipio_selecionado)  &
-    (df_estado["exercicio"] == exercicio_selecionado)
+    (df_estado["exercicio"] == exercicio_selecionado) &
+    (df_estado["dimensao"] == dimensao_selecionada) &
+    (df_estado["aplicavel"] == True)
 ].copy()
+
+df_media = calcular_media_estadual(df, exercicio_selecionado, estado_selecionado)
+
+df_exibicao = df_municipio.merge(
+    df_media,
+    on="verificacao",
+    how="left"
+)
+
+df_tabela = (
+    df_exibicao[
+        ["verificacao", "descricao", "nota", "media_estadual"]
+    ]
+    .style
+    .apply(destacar_nota, axis=1)
+)
+
+container = st.container(border=True)
+
+with container:
+    
+    
+    st.dataframe(
+        df_tabela,
+        hide_index=True,
+        column_config={
+            "verificacao":"Verificação",
+            "descricao":"Descrição",
+            "nota": st.column_config.NumberColumn("Nota", format="%.2f"),
+            "media_estadual": st.column_config.NumberColumn("Média estadual", format="%.2f")
+        },
+        use_container_width=True
+    )
+    
+        
+# df_dimensoes = df[
+#     (df["exercicio"] == exercicio_selecionado) &
+#     (df["aplicavel"] == True)
+# ]
+
+# dimensoes = sorted(df_dimensoes ["dimensao"].unique())
+
+# dimensao_selecionada = st.selectbox(
+#     "Dimensão",
+#     dimensoes
+# )
